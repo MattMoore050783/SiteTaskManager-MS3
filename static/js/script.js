912b1ce79318e175b579c708ba29a2c5e63a94ef
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2021-02-27 22:44:10 +0000

    add task form design completed

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $("select").formSelect();
+    $(".datepicker").datepicker({
+        format: "dd/mm/yyyy",
+        yearRange: 2,
+        showClearBtn: true,
+        i18n: {
+            done: "Confirm Date"
+        }
+    });
 });

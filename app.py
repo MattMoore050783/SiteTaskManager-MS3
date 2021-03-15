e01ca0baa3168768406368081d3dfdc0bdb66cef
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2021-03-15 20:29:21 +0000

    menu working from isAdmin added to session variable

--- app.py
+++ app.py
@@ -31,10 +31,11 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
+                        session["isAdmin"] = existing_user["isAdmin"]
                         flash("Welcome, {}".format(
                             request.form.get("username")))
                         return redirect(url_for(
-                            "profile", username=session["user"]))
+                            "profile", username=session["user"], existing_user=existing_user))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
